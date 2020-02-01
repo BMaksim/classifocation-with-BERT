20df5df7f69101464d33c2ab0f1bf4a28d3f6412
BMaksim <32644900+BMaksim@users.noreply.github.com>
2020-02-01 21:10:28 +0600

    back upgrade

--- back/app/routes.py
+++ back/app/routes.py
@@ -2,10 +2,16 @@
 from app import app
 from flask import render_template
 from app.forms import DataSendingForm
+from flask import render_template, flash, redirect
 
 
 @app.route('/')
-@app.route('/data')
+@app.route('/data', methods=['GET', 'POST'])
 def data():
     form = DataSendingForm()
+    if form.validate_on_submit():
+        flash('Just a second')
+        data = form.data.data
+        answer = get_answer(data)
+        return render_template('answer.html', answer = answer)
     return render_template('index.html', title = 'Hm...', form = form)
